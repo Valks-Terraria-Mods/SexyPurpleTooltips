07d171d5cdf3bfd81f34a6c80d8de5e9066622e8
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-06-06 11:43:59 -0500

    Use Dict<string, Color>

--- Tooltips.cs
+++ Tooltips.cs
@@ -2,69 +2,50 @@
 
 public class Tooltips : GlobalItem
 {
+    private static readonly Dictionary<string, Color> colors = new()
+    {
+        { "default", new Color(255, 153, 255) },
+        { "first", new Color(255, 77, 255) },
+        { "damage", new Color(179, 0, 179) },
+        { "critical", new Color(255, 0, 191) },
+        { "speed", new Color(204, 51, 255) },
+        { "knockback", new Color(255, 77, 148) },
+        { "power", new Color(153, 51, 255) },
+        { "life", new Color(255, 51, 153) },
+        { "consumable", new Color(255, 153, 204) },
+        { "material", new Color(255, 153, 190) },
+        { "placed", new Color(255, 153, 180) }
+    };
+
+    private static void SetColor(TooltipLine line, string type) 
+    {
+        if (line.Text.Contains(type))
+            line.OverrideColor = colors[type];
+    }
+
     public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
     {
         for (int i = 0; i < tooltips.Count; i++)
         {
-            TooltipLine line = tooltips[i];
+            var line = tooltips[i];
 
-            line.OverrideColor = new Color(255, 153, 255); // default color
+            line.OverrideColor = colors["default"];
 
-            if (i == 0)
-            {
-                line.OverrideColor = new Color(255, 77, 255);
-            }
+            if (i == 0) 
+                line.OverrideColor = colors["first"];
 
-            if (line.Text.Contains("damage"))
-            {
-                line.OverrideColor = new Color(179, 0, 179);
-            }
-
-            if (line.Text.Contains("critical"))
-            {
-                line.OverrideColor = new Color(255, 0, 191);
-            }
-
-            if (line.Text.Contains("speed"))
-            {
-                line.OverrideColor = new Color(204, 51, 255);
-            }
-
-            if (line.Text.Contains("knockback"))
-            {
-                line.OverrideColor = new Color(255, 77, 148);
-            }
-
-            if (line.Text.Contains("power"))
-            {
-                line.OverrideColor = new Color(153, 51, 255);
-            }
-
-            if (line.Text.Contains("life"))
-            {
-                line.OverrideColor = new Color(255, 51, 153);
-            }
-
-            if (line.Text.Contains("consumable"))
-            {
-                line.OverrideColor = new Color(255, 153, 204);
-            }
-
-            if (line.Text.Contains("material"))
-            {
-                line.OverrideColor = new Color(255, 153, 190);
-            }
-
-            if (line.Text.Contains("placed"))
-            {
-                line.OverrideColor = new Color(255, 153, 180);
-            }
+            SetColor(line, "damage");
+            SetColor(line, "critical");
+            SetColor(line, "speed");
+            SetColor(line, "knockback");
+            SetColor(line, "power");
+            SetColor(line, "life");
+            SetColor(line, "consumable");
+            SetColor(line, "material");
+            SetColor(line, "placed");
 
             if (line.IsModifier)
-            {
                 line.OverrideColor = new Color(204, 51, 255);
-            }
-
         }
     }
 }
